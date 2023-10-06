7ae67489fac1025c6be13264e0a34248feb1f909
laioAndrade <laioand98@gmail.com>
2023-10-06 12:04:45 -0300

    feat: add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <img src="../assets/quasar_logo.png" alt="quasar logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 15px 40px;
+  position: sticky;
+}
+
+header img {
+  max-height: 70px;
+}
+
+@media (max-width: 420px) {
+  header {
+    justify-content: center;
+  }
+}
+</style>

f388bf1d288c7c6208b86ed29094dd6c86ca3004
laioAndrade <laioand98@gmail.com>
2023-10-06 12:05:31 -0300

    feat: add App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Questrial, sans-serif;
+  font-size: 16px;
+}
+</style>

feb4ed45e82a123da3a905c8b0a1c7373bc70c8c
laioAndrade <laioand98@gmail.com>
2023-10-06 12:05:15 -0300

    feat: add Home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Map from '../components/Map.vue'
+</script>
+
+<template>
+  <main>
+    <Map />
+  </main>
+</template>
+
+<style scoped>
+main {
+  background: #232343;
+}
+</style>
